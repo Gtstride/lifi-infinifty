e0c998e179273eda8e5d514f5ac7728048ec8c5d
callysavio <callistusanwara@gmail.com>
2025-06-05 10:04:34 +0100

    folder structure and header implemented

---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./styles/header.css">
+    <link rel="stylesheet" href="./styles/footer.css">
+</head>
+
+<body>
+    <header>
+        <img id="logo" src="./assets/images/logo.svg" alt="LiFi logo">
+        <ul id="nav-ul">
+            <li class="nav-list"><a href="./index.php">Home</a></li>
+            <li class="nav-list"><a href="./about-us.php">About Us</a></li>
+            <li class="nav-list"><a href="./products.php">Products</a></li>
+            <li class="nav-list"><a href="./services.php">Services</a></li>
+            <li class="nav-list"><a href="./contact-us.php">Contact Us</a></li>
+        </ul>
+        <button>Partner With Us</button>
+    </header>
