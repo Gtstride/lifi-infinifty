672b8d1f7796aecb0356fa3f2762ff4240aabb0a
callysavio <callistusanwara@gmail.com>
2025-06-06 11:00:21 +0100

    footer implemented with responsiveness

---
+++ contact-us.php
@@ -0,0 +1,10 @@
+<?php
+include "./includes/header.php";
+?>
+<title>Lifi Inifinty - Contact Us</title>
+<div style="margin-top: 500px">
+    <h1>Contact Us Page</h1>
+</div>
+<?php
+include "./includes/footer.php";
+?>
--- services.php
+++ services.php
@@ -1,10 +1,10 @@
 <?php
 include "./includes/header.php";
 ?>
-<title>Our Services</title>
-<section>
-
-</section>
+<title>Lifi Inifinty - Services</title>
+<div style="margin-top: 500px">
+    <h1>Services Page</h1>
+</div>
 <?php
 include "./includes/footer.php";
 ?>
